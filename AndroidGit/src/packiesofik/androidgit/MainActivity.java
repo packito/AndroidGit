c80f100a86009beb9528fe924b9082bdbbf49aa7
Antonín Češík <packito1@gmail.com>
2013-05-11 17:02:54 +0200

    added few bugs

--- AndroidGit/src/packiesofik/androidgit/MainActivity.java
+++ AndroidGit/src/packiesofik/androidgit/MainActivity.java
@@ -16,6 +16,9 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		// TODO lkjfhsjkhdflsk
+		
+		// FIXME shit's not working
 		return true;
 	}
 
